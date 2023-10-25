b11407afaf90fd8708cc5e57a33d785216bf100c
JeffersoNascimento <jeffersonn561@gmail.com>
2023-10-25 16:58:04 -0300

    atualização

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.getElementById("cliente").addEventListener("input", function() {
     this.value = this.value.toUpperCase();
 });
@@ -11,36 +10,55 @@ document.getElementById("observacoes").addEventListener("input", function() {
     this.value = this.value.toUpperCase();
 });
 
-const cliente = document.getElementById("cliente").addEventListener("input", function() {
-    this.value = this.value
-})
+document.getElementById("responsavel").addEventListener("input", function(){
+    this.value = this.value.toUpperCase();
+});
 
-const btnGenerate = document.querySelector("#gerarPDF")
+const textarea = document.getElementById("ambiente");
+const textarea2 = document.getElementById("observacoes");
+
+// Atualize a altura do textarea sempre que houver uma mudança no conteúdo
+textarea.addEventListener('input', function() {
+  this.style.height = 'auto'; // Redefina a altura para auto
+  this.style.height = (this.scrollHeight) + 'px'; // Defina a altura com base no conteúdo
+});
+
+// Execute isso na inicialização para que o `textarea` ajuste sua altura com base no conteúdo inicial
+textarea.dispatchEvent(new Event('input'));
+
+textarea2.addEventListener('input', function() {
+    this.style.height = 'auto'; // Redefina a altura para auto
+    this.style.height = (this.scrollHeight) + 'px'; // Defina a altura com base no conteúdo
+  });
+
+textarea2.addEventListener(new Event('input'));
+
+
+const btnGenerate = document.querySelector("#gerarPDF");
 
 btnGenerate.addEventListener("click", () => {
+    // Obter o valor do campo "cliente" quando o botão é clicado
+    const cliente = document.getElementById("cliente").value;
 
-    //Conteúdo do pdf
-    const content = document.querySelector("#content")
+    // Obter a data atual e formatá-la como "dd-mm-aaaa"
+    const currentDate = new Date();
+    const day = currentDate.getDate().toString().padStart(2, '0'); // Formato de dois dígitos
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Mês começa em 0
+    const year = currentDate.getFullYear();
 
-    //Configuração do arquivo final do pdf
+    const data = `${day}-${month}-${year}`;
+
+    // Conteúdo do PDF
+    const content = document.querySelector("#content");
+
+    // Configuração do arquivo final do PDF
     const options = {
-        filename: `Checklist_Produção_${cliente}.pdf`,
-        html2canvas: {scale: 2},
-        jsPDF: {unit: "mm", format: "a4", orientation: "portrait"},
-        200: "Conversão bem sucedida",
-        400: "Você não especificou um URL nem HTML para converter"
-}
+        filename: `Checklist_Produção_${cliente}_${data}.pdf`,
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    };
 
-    //Gerar e baixar PDF
+    // Gerar e baixar o PDF
     html2pdf().set(options).from(content).save();
-})
-
-
-
-
-
-
-
-  
-  
-
+   
+});
